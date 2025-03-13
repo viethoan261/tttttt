9a5919017ed9b1804008b9a53c9a41045eb9c580
Nguyễn Việt Hoàn <95644536+viethoan261@users.noreply.github.com>
2025-03-13 22:11:56 +0700

    s

--- WebFilm/Controllers/ReportsController.cs
+++ WebFilm/Controllers/ReportsController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.IO;
 using WebFilm.Core.Interfaces.Services;
 
 namespace WebFilm.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ReportsController : ControllerBase
